fea06bdc9d56251cbdf1281aa9abea3bc2fa9033
unknown <bathariagung@google.com>
2025-04-30 00:22:36 +0700

    feat(navbar): tambah navbar dekstop

---
+++ components/layouts/hero.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className='bg-cover bg-no-repeat h-[114vh] w-full bg-center flex items-center justify-center' style={{backgroundImage: "url('/img/ter1.png')"}}>
+        <h1 className='text-amber-100'>hero</h1>
+    </div>
+  )
+}
+
+export default Hero
